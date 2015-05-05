b1f444a00505bdf23d820dfac4baa88868ec1dac
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2015-05-05 18:54:53 +0300

    main.c: made more tunning and bugfixes

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -14,8 +16,25 @@
 char IP[16];	// adresa IP de cautare server web; se va incrementa iterativ
 
 unsigned char fillRand(){
-	srandom((unsigned int) time(0));
-	return (unsigned char) (random() % 256);
+	int fd_devurand, readcount;
+	char read_buf;
+
+	fd_devurand = open("/dev/urandom", O_RDONLY);
+	if(fd_devurand == -1){
+		perror("open /dev/urandom");
+		exit(EXIT_FAILURE);	// v. pthread_exit
+	}	
+	readcount = read(fd_devurand, &read_buf, 1);
+	if(readcount != 1){
+		perror("read /dev/urandom");;
+		exit(EXIT_FAILURE);	// v. pthread_exit
+	}
+	if(close(fd_devurand) == -1){
+		perror("close /dev/urandom");
+		exit(EXIT_FAILURE);	// v. pthread_exit
+	}
+
+	return (unsigned char) read_buf;
 }
 
 void setIP(){
@@ -34,7 +53,7 @@ void setIP(){
 	b = fillRand();
 	c = fillRand();
 	d = fillRand();
-	while (a == 10 || (a == 172 && (b >= 16 && b <= 31)) || (a == 192 && b == 168) || (a == 127 && b == 0 && c == 0 && d == 1)){
+	while (a == 10 || (a == 172 && (b >= 16 && b <= 31)) || (a == 192 && b == 168) || a == 127){
 		bzero((void *)sa, 4);
 		bzero((void *)sb, 4);
 		bzero((void *)sc, 4);
@@ -46,14 +65,17 @@ void setIP(){
 		d = fillRand();
 	}
 	
-	sprintf(sa, "%d", (int)a);
+	sprintf(sa, "%d", (int)a);	// possible reentrancy issues
 	sprintf(sb, "%d", (int)b);
 	sprintf(sc, "%d", (int)c);
 	sprintf(sd, "%d", (int)d);
 
 	strcat(IP, sa);
+	strcat(IP, ".");
 	strcat(IP, sb);
+	strcat(IP, ".");
 	strcat(IP, sc);
+	strcat(IP, ".");
 	strcat(IP, sd);
 
 	//strcpy(IP,"192.168.3.1");
@@ -61,21 +83,31 @@ void setIP(){
 
 void *rutina_fir1(void *params)
 {
-	int sockfd, retcon;
+	int sockfd, retcon, flags;
 	struct sockaddr_in dest_addr;
 	
 
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd == -1){
-		perror("socket");
-		exit(EXIT_FAILURE);	// v. pthread_exit
-	}
-
-
 	while(1){
 		setIP();	
-		
+
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if(sockfd == -1){
+			perror("socket");
+			exit(EXIT_FAILURE);	// v. pthread_exit
+		}
+	
+		flags = fcntl(sockfd, F_GETFL);
+		if(flags == -1){
+			perror("fcntl");
+			exit(EXIT_FAILURE);	// v. pthread_exit
+		}
+		flags |= O_NONBLOCK;;
+		if(fcntl(sockfd, F_SETFL, flags) == -1){
+			perror("fcntl F_SETFL");
+			exit(EXIT_FAILURE);	// v. pthread_exit
+		}
+
 		dest_addr.sin_family = AF_INET;
 		dest_addr.sin_port = htons(DEST_PORT);
 		dest_addr.sin_addr.s_addr = inet_addr(IP);
@@ -93,12 +125,28 @@ void *rutina_fir1(void *params)
 	
 	// processing here ...
 
-	
+		if(!retcon){
+			char writebuf[29];
+		
+			memset(writebuf, 0, 29);
+			strcat(writebuf, "Connected to ");	// possible reentrancy issues!
+			strcat(writebuf, IP);
+			strcat(writebuf, "\n");
+			write(1, writebuf, 14 + strlen(IP));
+			sleep(2);
+		}
+
 		if(!retcon && (shutdown(sockfd,SHUT_RDWR) == -1)){
+			
 			perror("shutdown");
 			exit(EXIT_FAILURE);	// v. pthread_exit
 		}
-	
+		
+		if(close(sockfd) == -1){
+			perror("close");
+			exit(EXIT_FAILURE);	// v. pthread_exit
+		}
+
 		//sleep((unsigned int)1);
 	}
 	return NULL;
