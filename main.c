246391a015bdf2b2a84488c3b86eb27f181f52ce
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2015-05-11 21:46:42 +0300

    main.c: code optimizations

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #define DEST_PORT 80
 
 // macro used on system calls errors; on final version suppress memset() call here and use it only once at beginning; adjust temp[] dimension as needed 
-#define ERROR(msg)	memset(temp, 0, 32); sprintf(temp, "[%s]:%d " msg, __FILE__, __LINE__); write(STDOUT_FILENO, temp, strlen(temp));
+#define ERROR(msg)	memset(temp, 0, 32); sprintf(temp, "[%s]:%d " msg "\n", __FILE__, __LINE__); write(STDOUT_FILENO, temp, strlen(temp)); perror(msg);
 
 
 char IP[16];	// adresa IP de cautare server web; se va incrementa iterativ
@@ -28,19 +28,16 @@ unsigned char fillRand(){
 
 	fd_devurand = open("/dev/urandom", O_RDONLY);
 	if(fd_devurand == -1){
-		perror("open /dev/urandom");
-		ERROR("open\n");
+		ERROR("open");
 		exit(EXIT_FAILURE);	// v. pthread_exit
 	}	
 	readcount = read(fd_devurand, &read_buf, 1);
 	if(readcount != 1){
-		perror("read /dev/urandom");;
-		ERROR("read\n");
+		ERROR("read");
 		exit(EXIT_FAILURE);	// v. pthread_exit
 	}
 	if(close(fd_devurand) == -1){
-		perror("close /dev/urandom");
-		ERROR("close\n");
+		ERROR("close");
 		exit(EXIT_FAILURE);	// v. pthread_exit
 	}
 
@@ -105,21 +102,18 @@ void *rutina_fir1(void *params)
 
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if(sockfd == -1){
-			perror("socket");
-			ERROR("socket\n");
+			ERROR("socket");
 			exit(EXIT_FAILURE);	// v. pthread_exit
 		}
 	
 		flags = fcntl(sockfd, F_GETFL);
 		if(flags == -1){
-			perror("fcntl");
-			ERROR("fcntl\n");
+			ERROR("fcntl");
 			exit(EXIT_FAILURE);	// v. pthread_exit
 		}
 		flags |= O_NONBLOCK;;
 		if(fcntl(sockfd, F_SETFL, flags) == -1){
-			perror("fcntl F_SETFL");
-			ERROR("fcntl\n");
+			ERROR("fcntl");
 			exit(EXIT_FAILURE);	// v. pthread_exit
 		}
 
@@ -133,8 +127,7 @@ void *rutina_fir1(void *params)
 			if(errno == ECONNREFUSED){
 				write(STDOUT_FILENO, "Connection refused\n", 19);
 				if(close(sockfd) == -1){
-					perror("close");
-					ERROR("close\n");
+					ERROR("close");
 					exit(EXIT_FAILURE);	// v. pthread_exit
 				}
 				continue;
@@ -160,14 +153,12 @@ void *rutina_fir1(void *params)
 			
 			while(1){
 				if(select(sockfd + 1, NULL, &wfds,  NULL, &tv) == -1){
-					perror("select");
-					ERROR("select\n");
+					ERROR("select");
 					exit(EXIT_FAILURE);	// v. pthread_exit
 				}
 				if(FD_ISSET(sockfd, &wfds)){
 					if(getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &sockoptval, &sockoptsize) == -1){
-						perror("getsockopt");
-						ERROR("getsockopt\n");
+						ERROR("getsockopt");
 						exit(EXIT_FAILURE);	// v. pthread_exit
 					}
 					if(!sockoptval){
@@ -176,16 +167,14 @@ void *rutina_fir1(void *params)
 						strcat(writebuf, IP);
 						strcat(writebuf, "\n");
 						if(write(STDOUT_FILENO, writebuf, 14 + strlen(IP)) == -1){
-							perror("write");
-							ERROR("write\n");
+							ERROR("write");
 							exit(EXIT_FAILURE);	// v. pthread_exit or _exit()
 						}
 						
 						// writting on the socket
 						if(write(sockfd, "GET / HTTP/1.0\n\n", 16) == -1){
 							if(write(STDOUT_FILENO, "Error writing on socket\n", 24) == -1){
-								perror("write");
-								ERROR("write\n");
+								ERROR("write");
 								exit(EXIT_FAILURE);		// v. pthread_exit
 							}
 							break;
@@ -196,8 +185,7 @@ void *rutina_fir1(void *params)
 						tv.tv_sec = 5;
 						tv.tv_usec = 0;
 						if(select(sockfd+1, &rfds, NULL, NULL, &tv) == -1){
-							perror("select");
-							ERROR("select\n");
+							ERROR("select");
 							exit(EXIT_FAILURE);	// v. pthread_exit
 						}
 						if(FD_ISSET(sockfd, &rfds)){
@@ -205,20 +193,17 @@ void *rutina_fir1(void *params)
 
 							// open and create file "ip-index-html"
 							if((ipIndexHtml_fd = open("ip-index-html", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)) == -1){
-								perror("open");
-								ERROR("open\n");
+								ERROR("open");
 								exit(EXIT_FAILURE);	// v. pthread_exit
 							}
 							
 							// write IP address on first line of the file
 							if(write(ipIndexHtml_fd, IP, strlen(IP)) == -1){
-								perror("write");
-								ERROR("write\n");
+								ERROR("write");
 								exit(EXIT_FAILURE);	// v. pthread_exit
 							}
 							if(write(ipIndexHtml_fd, "\n", 1) == -1){
-								perror("write");
-								ERROR("write\n");
+								ERROR("write");
 								exit(EXIT_FAILURE);	// v. pthred_exit or _exit
 							}
 							
@@ -228,39 +213,37 @@ void *rutina_fir1(void *params)
 							while((read_count = read(sockfd, buf_read, 8192)) != 0){
 								if(read_count == -1){
 									if(errno == EAGAIN){
-										write(1, "Resource temporary anavailable, continuing...\n", 46);
+										if(write(STDOUT_FILENO, "Resource temporary anavailable, continuing...\n", 46) == -1){
+											ERROR("write");
+											exit(EXIT_FAILURE);	// v. pthread_exit
+										}
 										sleep(3);
 										continue;
 									}
 									if(errno == ECONNRESET){
 										continue;
 									}
-									perror("read");
-									ERROR("read\n");
+									ERROR("read");
 									exit(EXIT_FAILURE);	// v. pthread_exit
 								}
 								
 								// write into the file
 								if(write(ipIndexHtml_fd, buf_read, read_count) != read_count){
-									perror("write");
-									ERROR("write\n");
+									ERROR("write");
 									exit(EXIT_FAILURE);	// v. pthread_exit
 								}
 							}
 							
 							if(write(STDOUT_FILENO, "Written to ip-index-html\n", 25) == -1){
-								perror("write");
-								ERROR("write\n");
+								ERROR("write");
 								exit(EXIT_FAILURE);		// v. pthread_exit, _exit...
 							}	
 							if(close(ipIndexHtml_fd) == -1){
-								perror("close");
-								ERROR("close\n");
+								ERROR("close");
 								exit(EXIT_FAILURE);		// v. pthread_exit or _exit
 							}
 							if(errno != ECONNRESET && (shutdown(sockfd, SHUT_RDWR) == -1)){
-								perror("shutdown");
-								ERROR("shutdown\n");
+								ERROR("shutdown");
 								exit(EXIT_FAILURE);		// v. phread_exit
 							}
 							sleep(20);
@@ -274,8 +257,7 @@ void *rutina_fir1(void *params)
 		}
 
 		if(close(sockfd) == -1){
-			perror("close");
-			ERROR("close\n");
+			ERROR("close");
 			exit(EXIT_FAILURE);	// v. pthread_exit
 		}
 
@@ -290,15 +272,14 @@ int main(int argc, char *argv[])
 	pthread_t fir1;
 	
 	if(pthread_create(&fir1,NULL, &rutina_fir1, NULL)){
-		perror("pthread_create");
-		ERROR("pthread_create\n");
+		ERROR("pthread_create");
 		exit(EXIT_FAILURE);
 	}
 
 	// asteptam incheierea
 	if(pthread_join(fir1, NULL)){
-		perror("pthread_join");
-		ERROR("pthread_join\n");
+		ERROR("pthread_join");
+		exit(EXIT_FAILURE);
 	}
 
 	return EXIT_SUCCESS;
